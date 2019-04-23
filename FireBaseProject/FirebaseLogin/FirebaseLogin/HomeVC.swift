cc840e4f49c7747b6266174bc264b7312f4b9fe5
s1gnature <lamant0486@naver.com>
2019-04-23 20:07:29 +0900

    UserList_Database

--- FireBaseProject/FirebaseLogin/FirebaseLogin/HomeVC.swift
+++ FireBaseProject/FirebaseLogin/FirebaseLogin/HomeVC.swift
@@ -15,7 +15,6 @@ class HomeVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSo
     @IBOutlet var collectionView: UICollectionView!
     
     var Users: [UserVO] = []
-    var userUID: [String] = []
     
     // uid로 받아와야합니당
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -25,7 +24,7 @@ class HomeVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSo
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! collectionViewCell
         
-        cell.subjectLabel.text = Users[indexPath.row].uID!
+        cell.subjectLabel.text = Users[indexPath.row].subject!
         cell.explanationLabel.text = Users[indexPath.row].explanation!
         print(Users[indexPath.row].uID!)
         let data = try? Data(contentsOf: URL(string: Users[indexPath.row].imageURL!)!)
@@ -38,21 +37,34 @@ class HomeVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSo
     }
     
     func requestUserData(){
+        // value.key == users의 uID 이므로 allkeys에 대한 각각의 key 를 가져와서 리스트 구성
         DBRef.child("users").observeSingleEvent(of: .value, with: { (snapshot) in
             // Get user value
             // value == ALL OF users.child
             let value = snapshot.value as! NSDictionary
-            let child = value[(Auth.auth().currentUser?.uid)!] as! NSDictionary
-            let user = UserVO()
-            user.explanation = child["explanation"] as! String
-            user.uID = child["uID"] as! String
-            user.userID = child["userID"] as! String
-            user.subject = child["subject"] as! String
-            user.imageURL = child["imageURL"] as! String
+//            let child = value[(Auth.auth().currentUser?.uid)!] as! NSDictionary
+            for key in value.allKeys{
+                let child = value[key] as! NSDictionary
+                let user = UserVO()
+                user.explanation = child["explanation"] as! String
+                user.uID = child["uID"] as! String
+                user.userID = child["userID"] as! String
+                user.subject = child["subject"] as! String
+                user.imageURL = child["imageURL"] as! String
+                self.Users.append(user)
+            }
+            
+//            let user = UserVO()
+//            user.explanation = child["explanation"] as! String
+//            user.uID = child["uID"] as! String
+//            user.userID = child["userID"] as! String
+//            user.subject = child["subject"] as! String
+//            user.imageURL = child["imageURL"] as! String
+//            self.Users.append(user)
             
             //            user.uID = child["uID"] as! String
             //            print(user.uID)
-            self.Users.append(user)
+            
             //            let VO: NSDictionary = value(forKey: "\(Auth.auth().currentUser?.uid)")
             
             //            for child in snapshot.children{
