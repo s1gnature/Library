8282310ad970e89c0ee4e846461b2269d3703521
s1gnature <lamant0486@naver.com>
2019-05-14 17:43:52 +0900

    NaverMovie_Update

--- Senior/Senior5th_cocoapod/Pods/Target Support Files/ButtonBackgroundColor/ButtonBackgroundColor-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ButtonBackgroundColor : NSObject
-@end
-@implementation PodsDummy_ButtonBackgroundColor
-@end
--- Senior/Senior5th_cocoapod/Pods/Target Support Files/Pods-Senior5th_cocoapod/Pods-Senior5th_cocoapod-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Senior5th_cocoapod : NSObject
-@end
-@implementation PodsDummy_Pods_Senior5th_cocoapod
-@end
